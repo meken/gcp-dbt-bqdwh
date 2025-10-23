a2a7eb132584a0eefe963d7cf2ea516b9cc7af13
Azure DevOps Pipeline <azure-devops@pipeline.local>
2025-10-23 11:55:25 +0200

    Replace hand-made generator with dbt_utils version

--- models/marts/dim_date.sql
+++ models/marts/dim_date.sql
@@ -1,22 +1,31 @@
 {{ config(materialized='table') }}
 
-SELECT
-  {{ dbt_utils.generate_surrogate_key(["FORMAT_DATE('%F', d)"]) }} as date_key,
-  d AS full_date,
-  EXTRACT(YEAR FROM d) AS year,
-  EXTRACT(MONTH FROM d) AS month,
-  EXTRACT(DAY FROM d) AS day,
-  EXTRACT(WEEK FROM d) AS week_of_year,
-  EXTRACT(DAYOFYEAR FROM d) AS day_of_year,
-  EXTRACT(QUARTER FROM d) AS quarter_of_year,
-  EXTRACT(DAYOFWEEK FROM d) AS day_of_week,
-  FORMAT_DATE('%B', d) AS month_name,
-  FORMAT_DATE('%A', d) AS day_name,
-  (LAST_DAY(d) = d) AS is_last_day_of_month,
-  (EXTRACT(DAYOFWEEK FROM d) IN (1, 7)) is_weekend
-FROM (
-  SELECT
-    *
-  FROM
-    UNNEST(GENERATE_DATE_ARRAY('2001-01-01', '2030-01-01', INTERVAL 1 DAY)) AS d 
+with spine as (
+  {{ dbt_utils.date_spine(
+      datepart="day",
+      start_date="'2001-01-01'",
+      end_date="'2030-01-01'"
+  ) }}
+),
+
+formatted as (
+  select
+    date_day as d
+  from spine
 )
+
+select
+  {{ dbt_utils.generate_surrogate_key(["FORMAT_DATE('%F', d)"]) }} as date_key,
+  d as full_date,
+  EXTRACT(YEAR FROM d) as year,
+  EXTRACT(MONTH FROM d) as month,
+  EXTRACT(DAY FROM d) as day,
+  EXTRACT(WEEK FROM d) as week_of_year,
+  EXTRACT(DAYOFYEAR FROM d) as day_of_year,
+  EXTRACT(QUARTER FROM d) as quarter_of_year,
+  EXTRACT(DAYOFWEEK FROM d) as day_of_week,
+  FORMAT_DATE('%B', d) as month_name,
+  FORMAT_DATE('%A', d) as day_name,
+  (LAST_DAY(d) = d) as is_last_day_of_month,
+  (EXTRACT(DAYOFWEEK FROM d) IN (1, 7)) as is_weekend
+from formatted
